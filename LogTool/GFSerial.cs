bbd9d1b8e3114f102a61dec38fd3fe3f8cd313e1
GuoFan <guofan2@midea.com>
2020-08-28 15:41:03 +0800

    日志接收并显示可以

---
+++ LogTool/GFSerial.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace LogTool
+{
+    class GFSerial
+    {
+        static public PortsToDisplay ports = new PortsToDisplay();
+        static private DispatcherTimer timer_refresh = null;
+
+        public bool is_open = false;
+        public Mutex recv_data_mutex = new Mutex();
+        public List<byte> serial_recv_data = new List<byte>();
+
+        SerialPort serialPort = new SerialPort();
+        Action recv_callback = null;
+
+        public GFSerial(Action serial_recv_callback)
+        {
+            recv_callback = serial_recv_callback;
+
+            serialPort.ReadTimeout = 8000;
+            serialPort.WriteTimeout = 8000;
+            serialPort.ReadBufferSize = 1024;
+            serialPort.WriteBufferSize = 1024;
+            serialPort.Parity = Parity.None;
+            serialPort.DataBits = 8;
+            serialPort.StopBits = StopBits.One;
+            serialPort.Handshake = Handshake.None;
+            // serialPort.ReceivedBytesThreshold = 1; // 每1字节触发处理函数
+            serialPort.DataReceived += new SerialDataReceivedEventHandler(Serial_DataReceived);
+
+            if (timer_refresh == null) // todo multiple serial objects
+            {
+                timer_refresh = new DispatcherTimer();
+                timer_refresh.Interval = TimeSpan.FromMilliseconds(2000);
+                timer_refresh.Tick += new EventHandler(refresh_ports_callback);
+                timer_refresh.Start();
+            }
+        }
+
+        private void refresh_ports_callback(object sender, EventArgs e)
+        {
+            ports.Ports = SerialPort.GetPortNames().ToList();
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] reDatas = new byte[serialPort.BytesToRead];
+
+            serialPort.Read(reDatas, 0, reDatas.Length);
+
+            recv_data_mutex.WaitOne();
+            serial_recv_data.AddRange(reDatas);
+            recv_data_mutex.ReleaseMutex();
+            
+            if (recv_callback != null)
+            {
+                recv_callback.Invoke();
+            }
+        }
+
+        public bool open_serial(string com, int baud)
+        {
+            if (!ports.Ports.Any(e => e.StartsWith(com)))
+            {
+                return false;
+            }
+
+            serialPort.PortName = com;
+            serialPort.BaudRate = baud;
+
+            serialPort.Open();
+            is_open = true;
+
+            return true;
+        }
+
+        public bool close_serial()
+        {
+            serialPort.DiscardInBuffer();
+            serialPort.DiscardOutBuffer();
+
+            serialPort.Close();
+            is_open = false;
+
+            return true;
+        }
+
+        public class PortsToDisplay : INotifyPropertyChanged
+        {
+            private List<string> ports;
+            public List<string> Ports
+            {
+                get { return ports; }
+                set
+                {
+                    if (ports != value)
+                    {
+                        ports = value;
+                        PropertyChanged(this, new PropertyChangedEventArgs("Ports"));
+                    }
+                }
+            }
+            public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        }
+    }
+}
--- LogTool/MainWindow.xaml.cs
+++ LogTool/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace LogTool
 {
@@ -23,6 +26,107 @@ namespace LogTool
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        GFSerial serial = null;
+        ObservableCollection<LogItem> log_data = new ObservableCollection<LogItem>();
+        Mutex buffer_mutex = new Mutex();
+        string log_buffer = "";
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            serial = new GFSerial(delegate { recv_data_callback(); });
+
+            cb_com.DataContext = GFSerial.ports;
+
+            cb_baud.SelectedIndex = 8;
+
+            log_data.Add(new LogItem() { Text = "Log Tool file begin" });
+            dg_log.DataContext = log_data;
+
+            DispatcherTimer timer_show_log = new DispatcherTimer();
+            timer_show_log.Interval = TimeSpan.FromMilliseconds(20);
+            timer_show_log.Tick += new EventHandler(log_show_callback);
+            timer_show_log.Start();
+        }
+
+        private void open_state()
+        {
+            btn_com_open.Content = "打开串口";
+            cb_com.IsEnabled = true;
+            cb_baud.IsEnabled = true;
+        }
+
+        private void close_state()
+        {
+            btn_com_open.Content = "关闭串口";
+            cb_com.IsEnabled = false;
+            cb_baud.IsEnabled = false;
+        }
+
+        private void btn_com_open_Click(object sender, RoutedEventArgs e)
+        {
+            if (serial.is_open)
+            {
+                try
+                {
+                    serial.close_serial();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("无法关闭串口");
+                    return;
+                }
+
+                open_state();
+            }
+            else
+            {
+                try
+                {
+                    serial.open_serial(cb_com.Text, int.Parse(cb_baud.Text));
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("无法打开串口");
+                    return;
+                }
+
+                close_state();
+            }
+        }
+
+        private void recv_data_callback()
+        {
+            serial.recv_data_mutex.WaitOne();
+            byte[] recv_data = serial.serial_recv_data.ToArray();
+            serial.serial_recv_data.Clear();
+            serial.recv_data_mutex.ReleaseMutex();
+
+            buffer_mutex.WaitOne();
+            log_buffer += Encoding.UTF8.GetString(recv_data).Replace("\r", "");
+            buffer_mutex.ReleaseMutex();
+        }
+
+        private void log_show_callback(object sender, EventArgs e)
+        {
+            buffer_mutex.WaitOne();
+            string[] recv_items = log_buffer.Split('\n');
+            log_buffer = "";
+            buffer_mutex.ReleaseMutex();
+
+            log_data.Last().Text += recv_items[0];
+            for (int i = 1; i < recv_items.Length; i++)
+            {
+                log_data.Add(new LogItem() { Text = recv_items[i] });
+            }
+
+            dg_log.ScrollIntoView(dg_log.Items[dg_log.Items.Count - 1]);
+        }
+
+        class LogItem
+        {
+            public string Text { get; set; }
         }
     }
 }
